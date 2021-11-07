640d148de19a58e999173cbd78d4ff650a8b9eac
David <david@krawaller.se>
2021-11-07 17:43:56 +0100

    zoom control

--- sage/components/Shell.tsx
+++ sage/components/Shell.tsx
@@ -1,5 +1,11 @@
 import Link from 'next/link'
-import React, { ReactNode } from 'react'
+import React, {
+  CSSProperties,
+  ReactNode,
+  useCallback,
+  useMemo,
+  useState,
+} from 'react'
 import { SageSettings } from '../configTypes'
 import { SageLink, SageResource } from '../processSource/processTypes'
 import { useAuthService, useCurrentAuth } from '../services/service.auth'
@@ -12,30 +18,51 @@ export type SageShellProps = {
   settings: SageSettings
 }
 
-import styles from './Shell.module.css'
+import css from './Shell.module.css'
 
 export const Shell = (props: SageShellProps) => {
   const { resource, children, linkMap } = props
   const authService = useAuthService()
   const currentUser = useCurrentAuth()
+  const [zoom, setZoom] = useState(0)
+  const handleZoomChange = useCallback((evt) => {
+    setZoom(Number(evt.target.value))
+  }, [])
+  const styles = useMemo<CSSProperties & { '--zoom': number }>(
+    () => ({
+      '--zoom': zoom + 1,
+    }),
+    [zoom]
+  )
   return (
-    <div className={styles.shell}>
-      <div className={styles.controls}>
-        <nav className={styles.nav}>
+    <div className={css.shell} style={styles}>
+      <div className={css.controls}>
+        <nav className={css.nav}>
           {resource.crumbs.map((id) => (
             <ResourceLink key={id} link={linkMap[id]} />
           ))}
           <ResourceLink link={linkMap[resource.id]} naked />
         </nav>
-        {currentUser ? (
-          <button onClick={authService.signOut}>
-            Log out {currentUser.displayName}
-          </button>
-        ) : (
-          <button onClick={authService.signInWithGithubPopup}>Log in</button>
-        )}
+        <div>
+          <input
+            className="zoomer"
+            type="range"
+            min="0"
+            max="2"
+            step=".1"
+            value={zoom}
+            onChange={handleZoomChange}
+          />
+          {currentUser ? (
+            <button onClick={authService.signOut}>
+              Log out {currentUser.displayName}
+            </button>
+          ) : (
+            <button onClick={authService.signInWithGithubPopup}>Log in</button>
+          )}
+        </div>
       </div>
-      <div className={styles.content}>{children}</div>
+      <div className={css.content}>{children}</div>
     </div>
   )
 }
