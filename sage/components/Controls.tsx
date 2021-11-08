7d400e01ecabf064d4eccf4adf6fcd2818b375dc
David <david@krawaller.se>
2021-11-08 17:49:20 +0100

    extract controls component

---
+++ sage/components/Controls.tsx
@@ -0,0 +1,54 @@
+import React, { useCallback } from 'react'
+import { SageSettings } from '../configTypes'
+import { useAuthService, useCurrentAuth } from '../services/service.auth'
+import { useCssVars } from './contexts'
+import css from './Controls.module.css'
+
+export type ControlsProps = {
+  settings: SageSettings
+}
+
+export const Controls = (props: ControlsProps) => {
+  const { settings } = props
+  const { cssVars, updateCssVars } = useCssVars()
+  const handleZoomChange = useCallback(
+    (e) => updateCssVars({ zoom: e.target.value }),
+    []
+  )
+  const {
+    controls: { zoomMin, zoomMax },
+    emojis,
+  } = settings
+  const authService = useAuthService()
+  const currentUser = useCurrentAuth()
+  return (
+    <div className={css.controls}>
+      <input
+        className="zoomer"
+        type="range"
+        min={zoomMin}
+        max={zoomMax}
+        step=".1"
+        value={cssVars['--zoom']}
+        onChange={handleZoomChange}
+      />
+
+      <button
+        onClick={() =>
+          document.fullscreenElement
+            ? document.exitFullscreen()
+            : document.documentElement.requestFullscreen()
+        }
+      >
+        {emojis.fullscreen} Fullscreen
+      </button>
+      {currentUser ? (
+        <button onClick={authService.signOut}>{emojis.login} Log out</button>
+      ) : (
+        <button onClick={authService.signInWithGithubPopup}>
+          {emojis.login} Log in
+        </button>
+      )}
+    </div>
+  )
+}
--- sage/components/Shell.tsx
+++ sage/components/Shell.tsx
@@ -1,9 +1,8 @@
-import React, { ReactNode, useCallback } from 'react'
+import React, { ReactNode } from 'react'
 import { SageSettings } from '../configTypes'
 import { SageLink, SageResource } from '../processSource/processTypes'
-import { useAuthService, useCurrentAuth } from '../services/service.auth'
 import { BreadCrumbs } from './Breadcrumbs'
-import { useCssVars } from './contexts'
+import { Controls } from './Controls'
 
 export type SageShellProps = {
   resource: SageResource
@@ -16,51 +15,11 @@ import css from './Shell.module.css'
 
 export const Shell = (props: SageShellProps) => {
   const { resource, children, linkMap, settings } = props
-  const {
-    controls: { zoomMin, zoomMax },
-    emojis,
-  } = settings
-  const authService = useAuthService()
-  const currentUser = useCurrentAuth()
-  const { cssVars, updateCssVars } = useCssVars()
-  const handleZoomChange = useCallback(
-    (e) => updateCssVars({ zoom: e.target.value }),
-    []
-  )
   return (
     <div className={css.shell}>
       <div className={css.topbar}>
         <BreadCrumbs linkMap={linkMap} resource={resource} />
-        <div className={css.controls}>
-          <input
-            className="zoomer"
-            type="range"
-            min={zoomMin}
-            max={zoomMax}
-            step=".1"
-            value={cssVars['--zoom']}
-            onChange={handleZoomChange}
-          />
-
-          <button
-            onClick={() =>
-              document.fullscreenElement
-                ? document.exitFullscreen()
-                : document.documentElement.requestFullscreen()
-            }
-          >
-            {emojis.fullscreen} Fullscreen
-          </button>
-          {currentUser ? (
-            <button onClick={authService.signOut}>
-              {emojis.login} Log out
-            </button>
-          ) : (
-            <button onClick={authService.signInWithGithubPopup}>
-              {emojis.login} Log in
-            </button>
-          )}
-        </div>
+        <Controls settings={settings} />
       </div>
       <div className={css.content}>{children}</div>
     </div>
